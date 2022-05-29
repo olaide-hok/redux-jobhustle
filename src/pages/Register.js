dc46ebdc6cffeddec8f76242a3bfd0fa8c52248a
olaide-hok <olaidekareem43@yahoo.com>
2022-05-29 14:51:25 +0100

    Setup FormRow component for Register Page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { FormRow, Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 function Register() {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Register</div>
-  )
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+
+        {/* Email Field */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        {/* Password Field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 }
 
-export default Register
+export default Register;
