80b68d0b5542e9e646eb1f4d3244b7b8da68b30f
olaide-hok <olaidekareem43@yahoo.com>
2022-05-31 12:17:29 +0100

    Setup Protectedd Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import { Landing, Error, Register } from './pages'
+import { Landing, Error, Register, ProtectedRoute } from './pages'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AddJob, AllJobs, Profile, SharedLayout, Stats } from './pages/dashboard';
@@ -9,7 +9,11 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<SharedLayout />}>
+      <Route path='/' element={
+        <ProtectedRoute>
+          <SharedLayout />
+        </ProtectedRoute>
+      }>
         <Route index element={<Stats />} />
         <Route path='all-jobs' element={<AllJobs />} />
         <Route path='add-job' element={<AddJob />} />
