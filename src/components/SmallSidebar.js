159a21b243f4be13e9447d4c4519ec9a2fcd3ab0
olaide-hok <olaidekareem43@yahoo.com>
2022-05-31 12:29:08 +0100

    Small sidebar setup

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+import { useSelector, useDispatch } from 'react-redux';
 
 const SmallSidebar = () => {
   return (
-    <Wrapper>SmallSidebar</Wrapper>
+    <Wrapper>
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button className='close-btn' onClick={() => console.log('toggle') }>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>nav links</div>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 
