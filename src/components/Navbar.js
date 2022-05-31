5557d1da2b0409202c71a0c486f4dd9b37b4cf43
olaide-hok <olaidekareem43@yahoo.com>
2022-05-31 08:40:04 +0100

    Add toggleSidebar functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import Wrapper from '../assets/wrappers/Navbar'
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
 import Logo from './Logo';
 import { useDispatch, useSelector } from 'react-redux';
+import { toggleSidebar } from '../features/user/userSlice';
 
 
 
@@ -10,13 +11,17 @@ const Navbar = () => {
   const { user } = useSelector((store) => store.user)
   const dispatch = useDispatch()
 
+  const toggle = () => {
+    dispatch(toggleSidebar())
+  }
+
   return (
     <Wrapper>
       <div className='nav-center'>
         <button
           type='button'
           className='toggle-btn'
-          onClick={() => console.log('toggle sidebar')}
+          onClick={toggle}
         >
           <FaAlignLeft />
         </button>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -5,6 +5,7 @@ import { addUserToLocalStorage, getUserFromLocalStorage } from '../../utils/loca
 
 const initialState = {
     isLoading: false,
+    isSidebarOpen: false,
     user: getUserFromLocalStorage(),
 }
 
@@ -36,6 +37,11 @@ export const loginUser = createAsyncThunk(
 const userSlice = createSlice({
     name: 'user',
     initialState,
+    reducers: {
+        toggleSidebar: (state) => {
+            state.isSidebarOpen = !state.isSidebarOpen
+        }
+    },
     extraReducers: {
         [registerUser.pending]: (state) => {
             state.isLoading = true
@@ -68,4 +74,5 @@ const userSlice = createSlice({
     }
 })
 
+export const { toggleSidebar } = userSlice.actions
 export default userSlice.reducer;
