be90ac1d5676f41e73e6a025cbbcc3067e0f2e68
olaide-hok <olaidekareem43@yahoo.com>
2022-05-31 07:05:05 +0100

    Setup SharedLayout for Dashboard Pages

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,14 @@
+import AddJob from "./AddJobs";
+import AllJobs from "./AllJob";
+import Profile from "./Profile";
+import SharedLayout from "./SharedLayout";
+import Stats from "./Stats";
+
+
+export {
+    Profile,
+    Stats,
+    AddJob,
+    AllJobs,
+    SharedLayout   
+}
