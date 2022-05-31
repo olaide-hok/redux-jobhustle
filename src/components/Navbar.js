84d9e4b522c653a9c5d670d48dc0964789c7c682
olaide-hok <olaidekareem43@yahoo.com>
2022-05-31 07:49:31 +0100

    Install react-icons library

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
+import { FaHome } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <Wrapper>Navbar</Wrapper>
+    <Wrapper>
+      <FaHome />
+    </Wrapper>
   )
 }
 
