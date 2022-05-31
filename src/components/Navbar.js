87bc75530d0bc25a7c12e9870ca6dd8b7d637ef0
olaide-hok <olaidekareem43@yahoo.com>
2022-05-31 08:28:33 +0100

    Install react-icons and setup Navbar structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,50 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
-import { FaHome } from 'react-icons/fa'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import Logo from './Logo';
+import { useDispatch, useSelector } from 'react-redux';
+
+
 
 const Navbar = () => {
+  const { user } = useSelector((store) => store.user)
+  const dispatch = useDispatch()
+
   return (
     <Wrapper>
-      <FaHome />
+      <div className='nav-center'>
+        <button
+          type='button'
+          className='toggle-btn'
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button 
+            type='button'
+            className='btn'
+            onClick={() => console.log('toggle logout dropdown') }
+          >
+            <FaUserCircle />
+            {user ? user.name : null}
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button 
+              type='button'
+              className='dropdown-btn'
+              onClick={() => console.log('logout user')}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
