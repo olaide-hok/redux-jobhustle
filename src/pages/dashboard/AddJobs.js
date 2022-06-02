4dca0d429f5c43c2c05daff6ea6da1e1de7a1c73
olaide-hok <olaidekareem43@yahoo.com>
2022-06-02 17:23:47 +0100

    Setup Addjobs layout structure

--- src/pages/dashboard/AddJobs.js
+++ src/pages/dashboard/AddJobs.js
@@ -1,8 +1,88 @@
 import React from 'react'
+import { FormRow } from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
 
 const AddJobs = () => {
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    editJobId
+  } = useSelector((store) => store.job)
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(!position || !company || !jobLocation) {
+      toast.error('Please fill out all fields')
+      return
+    }
+  }
+
+  const handleJobInput = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+
+    console.log(name, value);
+  }
+
   return (
-    <div>AddJobs</div>
+    <Wrapper>
+      <form className='form'>
+        <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+        <div className='form-center'>
+          {/* Position */}
+          <FormRow 
+            type='text'
+            name='position'
+            value={position}
+            handleChange={handleJobInput}
+          />
+
+          {/* Company */}
+          <FormRow 
+            type='text'
+            name='company'
+            value={company}
+            handleChange={handleJobInput}
+          />
+
+          {/* Job Location */}
+          <FormRow 
+            type='text'
+            name='jobLocation'
+            labelText='Job Location'
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+
+          <div className='btn-container'>
+            <button type='button' className='btn btn-block clear-btn'
+              onClick={() => console.log('clear values') }
+            >
+              clear
+            </button>
+
+            <button type='submit' className='btn btn-block submit-btn'
+              onClick={handleSubmit}
+              disabled={isLoading}
+            >
+              submit
+            </button>
+          </div>
+
+        </div>
+      </form>
+    </Wrapper>
   )
 }
 
