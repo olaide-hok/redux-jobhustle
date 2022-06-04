f41379daf5e5bd3d791eecaf065c48bc77bc08fe
olaide-hok <olaidekareem43@yahoo.com>
2022-06-04 18:02:00 +0100

    Setup Stats Container

---
+++ src/components/StatItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StatItem = () => {
+  return (
+    <div>StatItem</div>
+  )
+}
+
+export default StatItem
--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import StatsContainer, { ChartsContainer } from '../../components'
+import { StatsContainer, ChartsContainer } from '../../components'
+import Loading from '../../components/Loading'
 import { showStats } from '../../features/allJobs/allJobsSlice'
 
 const Stats = () => {
   const { isLoading, monthlyApplications } = useSelector((store) => store.allJobs)
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(showStats)
+    dispatch(showStats())
 
   }, [])
+
+  if(isLoading) {
+    return <Loading center />
+  }
 
   return (
     <>
