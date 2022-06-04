a986ed177e81ceb3fedbd6491025e46dda9b7987
olaide-hok <olaidekareem43@yahoo.com>
2022-06-04 09:26:09 +0100

    Setup Delete Job functionality

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -2,7 +2,9 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import customFetch from '../../utils/axios';
 import { getUserFromLocalStorage } from '../../utils/localStorage';
+import { getAllJobs, hideLoading, showLoading } from '../allJobs/allJobsSlice';
 import { logoutUser } from '../user/userSlice';
+
 
 const initialState = {
   isLoading: false,
@@ -20,7 +22,7 @@ const initialState = {
 export const createJob = createAsyncThunk(
   'job/createJob',
   async (job, thunkAPI) => {
-    
+
     try {
       const resp = await customFetch.post('/jobs', job, {
         headers: {
@@ -37,6 +39,27 @@ export const createJob = createAsyncThunk(
       return thunkAPI.rejectWithValue(error.response.data.msg)      
     }
 
+  }
+)
+
+export const deleteJob = createAsyncThunk(
+  'job/deleteJob',
+  async(jobId, thunkAPI) => {
+    thunkAPI.dispatch(showLoading())
+    console.log(jobId);
+
+    try {
+      const resp = await customFetch.delete(`/jobs/${jobId}`, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`
+        },
+      })
+      thunkAPI.dispatch(getAllJobs())
+      return resp.data.msg   
+    } catch (error) {
+      thunkAPI.dispatch(hideLoading())
+      return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
   }
 )
 
@@ -64,6 +87,12 @@ const jobSlice = createSlice({
       },
       [createJob.rejected]: (state, {payload}) => {
         state.isLoading = false
+        toast.error(payload)
+      },
+      [deleteJob.fulfilled]: (state, {payload}) => {
+        toast.success(payload)
+      },
+      [deleteJob.rejected]: (state, {payload}) => {
         toast.error(payload)
       }
     }
