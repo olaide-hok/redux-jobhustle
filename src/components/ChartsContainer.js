34ca050524a7134436b508a3c53688f19567f2b6
olaide-hok <olaidekareem43@yahoo.com>
2022-06-04 18:34:13 +0100

    Setup Charts Components

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useSelector} from 'react-redux'
+import Wrapper from '../assets/wrappers/ChartsContainer'
+import AreaChart from './AreaChart'
+import BarChart from './BarChart'
 
 const ChartsContainer = () => {
+    const [barChart, setBarChart] = useState(true)
+
+    const { monthlyApplications: data } = useSelector((store) => store.allJobs)
+
   return (
-    <div>ChartsContainer</div>
+      <Wrapper>
+          <h4>Monthly Applications</h4>
+          <button type='button' onClick={() => setBarChart(!barChart)}>
+              {barChart ? 'Area Chart' : 'Bar Chart'}
+          </button>
+
+          { barChart ? <BarChart data={data} /> : <AreaChart data={data} /> }
+
+      </Wrapper>
+    
   )
 }
 
