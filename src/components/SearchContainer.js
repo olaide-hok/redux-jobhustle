d5076360a3ab41df4057cb99a908d7c7d86f8e8a
olaide-hok <olaidekareem43@yahoo.com>
2022-06-05 08:18:17 +0100

    Complete Search Container

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { FormRow, FormRowSelect } from ".";
+import Wrapper from "../assets/wrappers/SearchContainer";
+import { useSelector, useDispatch } from "react-redux";
 
 const SearchContainer = () => {
-  return (
-    <div>SearchContainer</div>
-  )
-}
+  const {
+    isLoading,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+    sortOptions,
+  } = useSelector((store) => store.allJobs);
 
-export default SearchContainer
+  const { jobTypeOptions, statusOptions } = useSelector((store) => store.job)
+
+  const dispatch = useDispatch()
+
+  const handleSearch = (e) => {}
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return <Wrapper>
+    <form className="form">
+      <h4>search form</h4>
+      <div className="form-center">
+        {/* Search by position */}
+        <FormRow
+          type='text'
+          name='search'
+          value={search}
+          handleChange={handleSearch}
+        />
+        {/* Search by Status */}
+        <FormRowSelect
+          labelText='status'
+          name='searchStatus'
+          value={searchStatus}
+          handleChange={handleSearch}
+          list={['all', ...statusOptions]}
+        />
+
+        {/* Search by Type */}
+        <FormRowSelect
+          labelText='type'
+          name='searchType'
+          value={searchType}
+          handleChange={handleSearch}
+          list={['all', ...jobTypeOptions]}
+        />
+
+        {/* Sort */}
+        <FormRowSelect
+          name='sort'
+          value={sort}
+          handleChange={handleSearch}
+          list={sortOptions}
+        />
+
+        <button className="btn btn-block btn-danger" disabled={isLoading}
+          onClick={handleSubmit}
+        >Clear Filters</button>
+      </div>
+    </form>
+  </Wrapper>;
+};
+
+export default SearchContainer;
